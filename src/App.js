9a75420b6efe26dfb65dd0906855b26c42086b05
Ivan Szebenszki <szib77@gmail.com>
2019-11-30 16:25:15 +0000

    Add basic appbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React from 'react';
 import './App.css';
 
 import Position from './lib/Position';
-import Map from './Map/Map';
+import Map from './components/Map/Map';
+import AppBar from './components/AppBar/AppBar';
+
 const center = new Position(51.5049375, -0.0964509);
 
 function App() {
-  return <Map center={center} />;
+  return (
+    <>
+      <Map center={center} />;
+      <AppBar />
+    </>
+  );
 }
 
 export default App;
