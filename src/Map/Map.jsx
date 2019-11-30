511a6ef8dba5aaf13354f56f52d2af21f65c4b7b
Ivan Szebenszki <szib77@gmail.com>
2019-11-30 15:55:45 +0000

    Add Map

---
+++ src/Map/Map.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Map, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+const MainMap = ({ center }) => {
+  return (
+    <>
+      <Map center={center.toArray()} zoom={14} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
+        />
+      </Map>
+    </>
+  );
+};
+
+export default MainMap;
