d07022780bc311ccd47eef11667f613fb98b6239
Ivan Szebenszki <szib77@gmail.com>
2019-11-30 21:12:56 +0000

    Call api from App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+
+import useAxios, { configure } from 'axios-hooks';
+import axios from './lib/axios';
 
 import Position from './lib/Position';
 import Map from './components/Map/Map';
@@ -8,6 +11,21 @@ import AppBar from './components/AppBar/AppBar';
 const center = new Position(51.5049375, -0.0964509);
 
 function App() {
+  useEffect(() => {
+    configure({ axios });
+  }, []);
+
+  const body = {
+    latitude: center.lat,
+    longitude: center.long,
+    distance: 0.1
+  };
+
+  const [{ data: getData, loading: getLoading, error: getError }] = useAxios({
+    url: '/parkingspots',
+    params: body
+  });
+
   return (
     <>
       <Map center={center} />;
